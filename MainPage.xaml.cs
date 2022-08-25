a9aa87c9c65e7668148e050d215fe55a7edde944
Moe Thet <maymyomoe90@hotmail.com>
2022-08-26 00:18:31 +0630

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Todo.ViewModel;
+
+namespace Todo;
+
+public partial class MainPage : ContentPage
+{
+	
+	public MainPage(MainViewModel vm)
+	{
+		InitializeComponent();
+		BindingContext = vm;
+	}
+
+	
+}
+
